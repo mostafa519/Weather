42d81e5acfbc13118b5a9763d6a277e9929ccdcf
Mustafa Ali <engmostafa51999@gmail.com>
2024-03-02 14:41:45 +0200

    Weather navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
  
+import Navbar from './Component/Navbar/Navbar';
 import Weather from './Pages/Weather/Weather';
 
 function App() {
   return (
+    <>
+    <Navbar></Navbar>
  <Weather></Weather>
+ </>
   );
 }
 
